b59a62227d465e4989052481282e42ee3cc93315
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2020-01-11 14:21:14 +0530

    Add domain to DB updated

--- monitorx.py
+++ monitorx.py
@@ -10,18 +10,28 @@ client = MongoClient('mongodb://root:sg3043il@127.0.0.1:27017/')
 db = client.subdomains
 
 def addDomain():
+    mycol = db["targets"]
     domain = str(input("Enter the domain to Monitor: "))
-    #print(domain)
-    db.targets.insert(domain)
+    print("Domain %s is added to the target list" % (domain))
+    
+    #Getting key value from db and incrementing it by 1 to add next domain in order
 
+    cur = mycol.find().sort([('_id', -1)]).limit(1)
+    for doc in cur:
+        res = list(doc.keys())
+    key_index = str(int(res[-1])+1)
+    to_insert = {
+            key_index: domain
+            }
+    db.targets.insert_one(to_insert)
 
-def runScan():
-    #reconx = subprocess.call('./recon.sh')
+def runOneScan():
+    domain = str(input("Enter the domain to scan: "))
+    #reconx = subprocess.call(['./recon.sh', domain])
 
     #appending line numbers to convert to dict json
     with open('tedtest', 'r') as program:
         data = program.readlines()
-
     with open('outfile', 'w') as program:
         for (number, line) in enumerate(data):
             program.write('%d %s' % (number + 1, line))
@@ -36,10 +46,12 @@ def runScan():
     collection = db.target
     collection.insert_one(data)
 
+def runAllScan():
+    print("TEST")
 
 def main():
     while True:
-        print("MONITOR-X\n")
+        print("----------------MONITOR-X---------------------\n")
         print("[1] - Add a Domain to monitoring list\n[2] - Run the scan against a particular domain\n[3] - Run scans against all domains\n[4] - Exit\nSelect your option: ")
         choice = int(input(">>> "))
         if choice==1:
@@ -49,7 +61,10 @@ def main():
         elif choice==3:
             runAllScan()
         elif choice==4:
-            break()
+            print("Bye")
+            break
         else:
             print("Invalid Choice, please choose again\n")
 
+if __name__ == "__main__":
+    main()
